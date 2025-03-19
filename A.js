50d6d5c6519f4a8050dadc469a5c3530d837be35
Coldaewon <deawon04072@gmail.com>
2025-03-19 15:10:37 +0900

    aa

--- A.js
+++ A.js
@@ -1,7 +1,24 @@
+let texts = [];
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+  textSize(24);
+  textAlign(LEFT, TOP);
 }
 
 function draw() {
-  background(220);
+  background(0);
+  fill(255);
+  
+  let newText = localStorage.getItem("sharedText");
+  if (newText && !texts.includes(newText)) {
+    texts.push(newText);
+    localStorage.removeItem("sharedText");
+  }
+  
+  let y = 20;
+  for (let i = 0; i < texts.length; i++) {
+    text(texts[i], 20, y);
+    y += 30;
+  }
 }
